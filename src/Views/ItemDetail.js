f8b4fe1ea188d8a2b40ded41179067cd96afaa70
Nina Spinelli <ninafspin@gmail.com>
2021-06-28 22:29:24 -0300

    listo desafio clase 9 evento onAdd

--- src/Views/ItemDetail.js
+++ src/Views/ItemDetail.js
@@ -1,46 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ItemCount from '../components/Body/ItemCount';
 import './_ItemDetails.scss';
 import {Link} from 'react-router-dom';
 
-function propDetail({prop}) {
+function ItemDetail({prop}) {
+    const [quantity, setQuantity] = useState(0);
+    
+    const onAdd = (e) => {
+        setQuantity(e);
+    }
+
     return (
         <div>
-
-          <div className="prop card item-detail">
-              <img src={`${prop.image}`} alt="Imágen Producto"></img>
-              <p key={prop.id} className="prop-p">{prop.title} <span>${prop.price}</span></p>
-              <p className="prop-p-description">{prop.description}</p>
-              <ItemCount />
-              <Link to="/products"><p className="prop-p-volver">volver</p></Link>
-          </div>
-            {/*
-            <Card>
-      <Card.Content>
-        <img
-          src={`${prop.image}`}/>
-        <Card.Header>{prop.title}</Card.Header>
-        <Card.Meta><p>${prop.price}</p></Card.Meta>
-        <Card.Description>
-        <p>{prop.description}</p>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button onClick={()=>{swal(`${prop.title}`, 'Agregado al Carrito', 'success')}} basic color='green'>
-            Agregar
-          </Button>
-          <Link to='/'>
-          <Button basic color='red'>
-            Volver
-          </Button>
-          </Link>
-        </div>
-      </Card.Content>
-    </Card>
-            */}
+            <div className="prop card item-detail">
+                <img src={prop.image} alt="Imágen Producto"></img>
+                <p key={prop.id} className="prop-p">{prop.title} <span>${prop.price}</span></p>
+                <p className="prop-p-description">{prop.description}</p>
+                {quantity === 0 ? 
+                    <ItemCount stock={prop.stock} name={prop.title} onAdd={onAdd}/>
+                :   <div>
+                    <p><b>Compraste {quantity} {prop.title}</b></p>
+                    <Link to={'/cart'} ><button className="btn btn-secondary">Terminar compra</button></Link>
+                    </div>      
+                    
+                }
+                <Link to="/products"><p className="prop-p-volver">volver</p></Link>
+            </div>
         </div>
     )
 }
 
-export default propDetail;
+export default ItemDetail;
--- src/components/Body/ItemCount.js
+++ src/components/Body/ItemCount.js
@@ -1,55 +1,36 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './_ItemCount.scss';
 
-class ItemCount extends Component  {
-    constructor () {
-        super();
-        this.state = {
-            counter: 1,
-            stock: 10
-        }
+const ItemCount = ({stock, onAdd}) => {
+    const [quantity, setQuantity] = useState(1);
+    stock = 5;
+    const handlerPlus = () => {
+      setQuantity((quantity === stock) ? stock : quantity + 1 )
+    };
+    const handlerMinus = () =>{
+      setQuantity((quantity >= 2) ? quantity - 1 : quantity)
     }
 
-    handlerPlus = () => {
-        if(this.state.counter === this.state.stock) {
-            this.setState({
-                counter: this.state.stock
-            });
+    /*addItem = (quantity) => {
+        if(quantity === 1) {
+            alert("Compraste 1 producto")
         } else {
-            this.setState({counter: this.state.counter + 1})
+            alert(`Compraste ${quantity} productos`)
         }
-    }
+    }*/
 
-    handlerMinus = () => {
-        if(this.state.counter === 1){
-            this.setState({
-                counter: 1
-            });
-          } else {
-            this.setState({
-                counter: this.state.counter - 1
-            });
-          }
-    }
-
-    onAdd = () => {
-        alert("Producto agregado exitosamente");
-    }
-
-    render() {
-        return (
+    return (
             <div className="card card-main">
                 <div className="card-body">
                     <div className="card card-content">
-                        <p onClick={this.handlerMinus} className="plus-minus">-</p>
-                        <p id="counter-value">{this.state.counter}</p>
-                        <p onClick={this.handlerPlus} className="plus-minus">+</p>
+                        <p onClick={handlerMinus} className="plus-minus">-</p>
+                        <p id="counter-value">{quantity}</p>
+                        <p onClick={handlerPlus} className="plus-minus">+</p>
                     </div>
-                    <button className="btn btn-primary" onClick={this.onAdd}>Comprar</button>
+                    <button className="btn btn-primary" onClick={()=>onAdd(quantity)}>Comprar</button>
                 </div>
             </div>
-        )
-    }
+    )
 }
 
 export default ItemCount;
